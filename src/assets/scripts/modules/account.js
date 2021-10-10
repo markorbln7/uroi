dfe8f46b8bcff66bc1a01311e87c1df2df4a3448
Nemanja Blagojevic <nemanja@darwindigital.com>
2021-10-10 12:04:18 +0200

    my acc table

--- src/assets/scripts/modules/account.js
+++ src/assets/scripts/modules/account.js
@@ -3,12 +3,14 @@ export function initAccount() {
         const liArr = document.querySelectorAll('.my-acc__list-item');
         const orders = document.querySelector('[data-section="orders"]');
         const newsletter = document.querySelector('[data-section="newsletter"]');
+        const ordersLi = document.querySelector('[data-open="orders"]');
+        const newsletterLi = document.querySelector('[data-open="newsletter"]');
         const backBtns = document.querySelectorAll('.my-acc__back');
         const toggleCheckbox = document.querySelector('.toggle-checkbox');
 
         toggleCheckbox.addEventListener('click', () => {
             toggleCheckbox.classList.toggle('active');
-        })
+        }) 
 
         backBtns.forEach(btn => {
             btn.addEventListener('click', () => {
@@ -18,11 +20,16 @@ export function initAccount() {
 
         liArr.forEach(li => {
             li.addEventListener('click', () => {
+                document.querySelector('.my-acc__list-item--active').classList.remove('my-acc__list-item--active');
+                li.classList.add('my-acc__list-item--active');
+                
                 switch (li.dataset.open) {
                     case 'orders':
+                        newsletter.classList.remove('my-acc__tab--open');
                         orders.classList.add('my-acc__tab--open');
                         break;
                     case 'newsletter':
+                        orders.classList.remove('my-acc__tab--open');
                         newsletter.classList.add('my-acc__tab--open');
                         break;
                 
@@ -31,5 +38,14 @@ export function initAccount() {
                 }
             })
         })
+
+        const initTabOnDesk = () => {
+            if (window.innerWidth > 1200 && !document.querySelector('.my-acc__tab--open')) {
+                orders.classList.add('my-acc__tab--open');
+            }
+        }
+
+        initTabOnDesk();
+        window.addEventListener('resize', initTabOnDesk);
     }
 }
